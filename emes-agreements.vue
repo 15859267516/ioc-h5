143b4b3dc8d0ca5c7f26a89881a9b0e934da23c2
15859267516 <271743196@qq.com>
2021-10-09 14:03:55 +0800

    Add files via upload

---
+++ emes-agreements.vue
@@ -0,0 +1,93 @@
+<!--
+ * @Atuhor: Lee
+ * @Date: 2021-10-09 10:19:03
+ * @LastEditTime: 2021-10-09 14:03:22
+ * @FilePath: \vite\src\components\emes-agreements.vue
+-->
+<template>
+  <div class="page">
+    <h2>User Agreements</h2>
+    <h3>General Terms and Condition of EMES EvPro application</h3>
+    <p>Version dated 26.08.2021</p>
+    <h3>1. Definitions relating to the EMES EvPro Application</h3>
+    <p>The following definitions are used in this chapter. These definitions apply to the entire Contract.</p>
+    <p>1.1 Conditions: these general terms and conditions. including any appendices, relating to the purchase of the EMES EvPro App and access to Charging infrastructure.</p>
+    <p>1.2 Charge Point Operator: an operator of Charging infrastructure.</p>
+    <p>1.3 Charging infrastructure: Compatible Charging Sockets that are connected to EMES EvPro App.</p>
+    <p>1.4 Compatible Charging Socket a Charging Socket that is included in the database displayed in the EMES EvPro and which can be operated via the EMES EvPro App.</p>
+    <p>1.5 End-user: the natura! person or legal entity that has installed the EMES EvPro App and that may wish to have access to the Charging infrastructure</p>
+    <p>1.6 Electric Vehicle: A road vehicle powered entirely by an electric motor and/or a hybrid vehicle partly powered by an electric motor, which vehicle may or may not make use of electricity that is stored in a battery rechargeable by use of a Charging Socket.</p>
+    <p>1.7 Charging Socket a facility at a semi-public, public, or private location that can be used to charge the battery of an Electric Vehicle.</p>
+    <p>1.8 Charging Session: a session during which an Electric Vehicle is charged at one of the Compatible Charging Sockets using the EMES EvPro App.</p>
+    <p>1.9 Non-compatible Charging Socket a Charging Socket that is included in the database displayed in the EMES EvPro App but which cannot be operated via the EMES EvPro App.</p>
+    <p>1.10 Contract the agreement pursuant to which the Charging infrastructure is made available to the End-user via the EMES EvPro App, of which these general terms and conditions constitute an integral part.</p>
+    <p>1.11 EMES EvPro App, registered at 4, Ferjemannsveien 7041, Trondheim, Norway</p>
+    <p>1.12 EMES EvPro App: the iOS and Android application offered by EMES, based on which the End-user can access the Charging infrastructure to charge his/her Electric Vehicle.</p>
+    <p>1.13 Service Provider: a party with which a contract et for the supply of charging services is concluded.</p>
+    <h3>2. Installation, Operation, and Use of the EMES EvPro</h3>
+    <p>2.1 EMES distributes the EMES EvPro App via the Platform, from which the digital content can be downloaded. To be able to use the EMES EvPro APP, visit the Platform, where you will register with the Platform operator. That will bind you to the Platform's relevant terms and conditions of use and general terms and conditions. The contractual relationship between you and the operator regarding your use of the Platform is separate from the contractual relationship that you enter into directly with EMES for the use of the EMES EvPro App pursuant to the Contract. If there are problems with using the Platform. you must contact the Platform operator directly in its capacity as your counterparty. Clause 11 (exclusion of liability) applies mutatis mutandis to that contract.</p>
+    <p>2.2 The EMES EvPro can be downloaded onto a compatible mobile terminal that uses iOS or Android.</p>
+    <p>2.3 Before the EMES EvPro can be used, the End-user may open an account or log in as a guest. If a user account is opened, the End-user will, independently and at any time, be able to change his/her personal account settings, such as the data regarding the Electric Vehicle, charging card data and/or payment details. To open an account, the End-user must have a valid e-mail address.</p>
+    <h3>3. Right of Use</h3>
+    <p>3.1 EMES grants the End-user permission to use the EMES EvPro indefinitely to:</p>
+    <ul>
+        <li>• Find and review Compatible and Non-compatible Charging Sockets.</li>
+        <li>• Obtain access to Compatible Charging Sockets, such as by starting, stopping. and paying for the use of the Charging Sockets.</li>
+    </ul>
+    <p>3.2 The license to use the EMES EvPro App is non-transferable. The following actions are prohibited while using the application:</p>
+    <ul>
+        <li>• making the application available to third parties via a peer-to-peer or other network or in any other manner.</li>
+        <li>• leasing out or selling the application.</li>
+        <li>• granting a sub-license to use the application; and</li>
+        <li>• modifying the application, reverse engineering the source code, and/or using the application to develop derivative works.</li>
+    </ul>
+    <p>3.3 EMES shall be entitled to terminate the license immediately and at any time if the End-user violates these Conditions or misuses the application, or if the decision is taken to decommission the EMES EvPro App.</p>
+    <p>3.4 EMES shall be entitled to terminate the license immediately and at any time by sending an e-mail contact@emes-europe.com , requesting the deletion of the relevant account, upon which the right of use shall be terminate with immediate effect.</p>
+    <h3>4. Misuse of the Application</h3>
+    <p>4.1 Conditions, as well as using the EMES EvPro App in such a way that it infringes upon the rights of one or more third parties. Misuse of the application is a criminal offence. We may report misuse at any time to the police and/or public prosecutor's office.</p>
+    <h3>5. Use of information / Privacy</h3>
+    <p>5.1 By installing the EMES EvPro App, the end-user agrees to having the information he/she enters processed. stored, and used to the extent reasonably necessary for the proper operation of the EMES EvPro App, the further development of the EMES EvPro App, and the offering of access to the Charging infrastructure via the EMES EvPro App. EMES shall process, store, and use the information provided by the End-user in accordance with all applicable laws.</p>
+    <h3>6. Location Data</h3>
+    <p>6.1 For the EMES EvPro App to operate, EMES must be kept apprised of the End-user’s location. EMES can use this data to direct the End-user to a nearby Compatible Charging Socket To do so, EMES must be able to ascertain the End-user's location in real time. This data is tracked in anonymized form and is never stored.</p>
+    <h3>7. Indemnification</h3>
+    <p>7.1 By installing the EMES EvPro App, the End-user agrees to indemnify EMES, its directors, supervisory directors, employees, investors, contractors, and contractual counterparties against any liability to, or claims instituted by, third parties that arise as the direct or indirect consequence of the End-user's culpable failure to satisfy these Conditions. The End-user hereby waives any right of recovery he/she has in respect the aforementioned parties.</p>
+    <p>7.2 The End-user is aware of, and agrees that, he/she bears the risk of using the EMES EvPro App (EMES accepts no liability in respect of said use). The usefulness and quality of the EMES EvPro App depends on its being used properly by the End-user. EMES does not guarantee the operation or quality of the EMES EvPro, the correctness or accuracy of the information provided by the EMES EvPro App, or the speed or quality of any information provided in connection with the EMES EvPro App. EMES is not obliged to update or maintain the EMES EvPro App. The foregoing is related to. inter alia, the fact that the EMES EvPro App is dependent on the use of the Internet, as well as the possibility of receiving text and other messages and obtaining a GPS location, as well as the mobile telephone, mobile and other networks, and third-party services being in good working order. To facilitate the operation of the EMES EvPro, the End-user will be responsible for, inter alia. the following:</p>
+    <ul>
+        <li>• the correct provision of information, including (but not limited to) his/her e-mail address, information about his/her Electric Vehicle, and his/her charging card and payment details. The End-user must enter changes in this information himself/herself via his/her personal EMES EvPro App account;</li>
+        <li>• exercising due care in respect of his/her password for access to the EMES EvPro App and any access codes for using the compatible mobile telephone.</li>
+        <li>• misuse of the EMES EvPro App upon loss or theft of the compatible mobile telephone.</li>
+        <li>• the use of the GPS functionality in the EMES EvPro.</li>
+    </ul>
+    <h3>8. Changes to the Application</h3>
+    <p>8.1 EMES is entitled to change the EMES EvPro (for example, in the case of an update or when so prompted by changes in the application or to the applicable laws (or their interpretation), as well as to discontinue or terminate the app without any prior notice being required. EMES shall not be liable for any harm or loss that ensues, or is alleged to have ensued, from such change, discontinuation, or termination.</p>
+    <h3>9. Intellectual Property</h3>
+    <p>9.1 The End-user is aware that the EMES EvPro contains information of which EMES is the owner, including processes, work methods, software, drawings, texts, designs, and know- how, including but not limited to the rights referred to in the UK intellectual Property ACT 2014, the Benelux Convention on intellectual Property, the Industrial Property Office in Norway, the European Union Trademark Regulation, the European Patent Convention, the Uniform Benelux Act on Designs and Models, and the regulations relating to said acts (if not yet mentioned).</p>
+    <p>9.2. The End-user shall respect these rights and use the aforementioned information exclusively for the ordinary use of the application as described in these Conditions.</p>
+    <h3>10.	Charging</h3>
+    <p>10.1	The EMES EvPro App will enable the End-user to start, stop and schedule charging at the Charging Sockets.</p>
+    <p>10.2	When using the Charging Sockets, the End-User shall comply with all requirements applicable to charging; specifically, the requirements imposed by EMES, as well as any applicable statutory safety and other requirements.</p>
+    <p>10.3	EMES does not guarantee Charging Infrastructure density and/or the adequate availability of Charging Sockets. Neither does EMES guarantee that the Charging Sockets will be operational at all times without any disruption and/or failures or that these will be available.</p>
+    <h3>11. Liability</h3>
+    <p>11.1	The End-user shall be liable for any harm or loss resulting from the inexpert or careless use of a Charging Socket and shall indemnify EMES against any third-party claims relating thereto. The End- user shall also indemnify EMES against any third-party claims relating to conduct or circumstances for which the End-user bears the risk and/or expense.</p>
+    <p>11.2	EMES shall be liable for any harm or loss the End-user incurs as a consequence of EMES’s culpable failure to perform its contractual obligations to the End-user, subject to the condition that, within 10 business days after the date on which the End-user discovers, or could reasonably be expected to have discovered, the failure to perform or wrongful act, the End-user notifies EMES of same in writing, thereby, to the extent legally required, serving EMES with notice of default and affording EMES a reasonable time to perform its obligation or rectify its wrongful act.</p>
+    <p>11.3	The liability referred to in the previous paragraph of this clause shall – if and to the extent possible and with due observance of mandatory liability law- be limited to damages for direct harm or loss. EMES will not be liable for any loss of business, use, profile, anticipated profit, contracts, revenues, goodwill or anticipated savings; loss of data or use of data; or damage to the End-user's reputation or any costs relating thereto.</p>
+    <p>11.4	EMES shall not be liable for any harm or loss the End-user may incur because of being unable to charge (or fully charge) the Electric Vehicle or as a result of the use or operation of a Charging Socket. EMES shall not be liable if an Electric Vehicle cannot be charged (or safely charged) due to a defect in the Electric Vehicle and/or any of the auxiliary equipment used, such as charging cables.</p>
+    <p>11.5	The restrictions listed in the previous paragraphs of this clause shall lapse if and to the extent that the harm or loss is the consequence of an intentional act or omission or gross negligence on the part of EMES.</p>
+    <p>11.6	EMES shall not be held liable to preform any obligation if it is prevented from doing so because of force majeure.</p>
+    <h3>12. Other Provisions and Applicable Law</h3>
+    <p>12.1	EMES shall always be authorized to amend these Conditions on a unilateral basis.</p>
+    <p>12.2	In performing this Agreement, EMES shall be entitled to engage third parties or to assign all or part of the rights and obligations under this Agreement to a third party. The End-user hereby consents in advance to the assignment of its legal relationship with EMES to a third party.</p>
+    <p>12.3 If, for whatever reason, one or more provisions of these Conditions is invalid or null and void or is nullified, the remaining provisions shall remain in full force, and the Parties shall consult in order to put in place a replacement provision such that the intention and purport of the provision that is to be replaced is preserved as far as possible.</p>
+    <p>12.4	This Contract and these Conditions governed by and construed in accordance with the laws of Norway. Any dispute arising out or in connection with this Contract and these Conditions shall be subject to the exclusive jurisdiction of the competent court in Norway.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
