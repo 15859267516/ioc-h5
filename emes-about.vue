e46765723d8dd287f8e93566d071ce76745c88d3
15859267516 <271743196@qq.com>
2021-10-09 10:17:20 +0800

    Add files via upload

---
+++ emes-about.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Atuhor: Lee
+ * @Date: 2021-10-09 10:15:05
+ * @LastEditTime: 2021-10-09 10:16:49
+ * @FilePath: \vite\src\components\emes-about.vue
+-->
+<template>
+  <div class="page">
+    <p>EMES was created with a vision of shaping the future of electricity and to enable better personal mobility for the many. Located in the middle of Norway, where horizons stretch far, it’s clear to us that travel and exploration are a natural part of what makes us human. We’re passionate about finding and enabling better ways to travel.</p>
+    <p>At EMES we combine the latest tech with human-centred design to offer highly intuitive and efficient solutions that ensure easier access to personal electric transport.</p>
+    <p>Our goal is to make people's lives easier and more environmentally friendly by helping them use energy as efficiently as possible. Shaping the power grid of the future is not something we can do alone, and collaboration is key. We therefore understand the importance of building relationships and establishing partners to join us on our journey.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
