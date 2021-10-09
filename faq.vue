0f9235f5755bdccc9fffc8624f356d3c37c4fb26
15859267516 <271743196@qq.com>
2021-10-09 10:13:15 +0800

    Update faq.vue

--- faq.vue
+++ faq.vue
@@ -4,3 +4,54 @@
  * @LastEditTime: 2021-10-09 10:00:32
  * @FilePath: \vite\src\components\aa.vue
 -->
+<!--
+ * @Atuhor: Lee
+ * @Date: 2021-10-09 10:00:32
+ * @LastEditTime: 2021-10-09 10:12:15
+ * @FilePath: \vite\src\components\faq.vue
+-->
+<template>
+  <div>
+      <h2>The top model of EV charging stations – EvPro Smart</h2>
+      <p> A smart and efficient charging station with power ratings up to 22kW. EvPro Smart makes it easy and affordable to invest in a modern forward-looking charging infrastructure. Its lightning-intelligent, gives you more power, smarter control, and a high level of security. Built to comply with standard regulations, the EvPro Smart comes with integrated type B RCD protection. The charger is packed with state-of-the-art tech, and always online with Wi-Fi or 4G, EvPro Smart keeps itself up to date with the latest functions and updates. </p>
+      <ol>
+          <li>- Charge up to 22kW, which is up to 10x faster than with a regular outlet</li>
+          <li>- Universal, fits all types of electric cars and mains.</li>
+          <li>- Offline proof load balancing</li>
+          <li>- Integrated sim-card in each station</li>
+          <li>- Possibility of Wi-Fi connection</li>
+          <li>- Integrated earth fault protection (RCD)</li>
+          <li>- Tethered or untethered cable.</li>
+          <li>- Integrated RFID reader</li>
+          <li>- Full control with EvPro Home app.</li>
+          <li>- LCD display, get an overview and control the charging process.</li>
+      </ol>
+
+      <h2>FAQ</h2>
+      <h3>Is the EvPro Smart ‘Weather-proof’?</h3>
+      <p>The EvPro System Smart is made with a high-quality material to protect it from elements and is rated at IP54 for water ingress. That means it will perform perfectly well in Nordic climate.</p>
+      <h3>I got a 22kW charger, why does my car not charge with these power ratings?</h3>
+      <p>A 22kW charger is the fastest charger you can get for the home, but to achieve this the charge point will need to be three-phase compatible and use what’s called a three-phase electricity supply. In addition, the car itself will need to be able to accept 22kW. </p>
+      <h3>Can I get a different cable length for the EvPro Smart?</h3>
+      <p>Yes, we got different lengths and types of cables. If you want a longer untethered cable than the standard tethered 5m cable, you can choose between a longer 7.5m or a 10m cable.</p>
+      <h3>What is the warranty for the EvPro Smart?</h3>
+      <p>There is a five-year warranty on the EvPro Smart supplied by EMES. Any issues under warranty should be dealt with by EMES direct.</p>
+      <h3>What do smart charging stations offer that the others don’t?</h3>
+      <p>When a vehicle is smart charging, the charger is essentially communicating with your car, the charging operator, and the utility company through data connections. In other words, whenever you plug in your EV, the charger automatically sends them important data so they can optimize charging. Smart charges give you access to a variety of functions that others don’t. For example, the EvPro Smart allow you to see your charging data, remotely start your charging and control the access to your charger with an app on your smart phone.</p>
+      <h3>What is full dynamic load balancing?</h3>
+      <p>Full dynamic load balancing allows you to charge as fast as possible, protects the main fuse and ensures that you can use your electricity wherever it’s needed.</p>
+      <h3>What’s the difference between Type 1 and Type 2 connectors?</h3>
+      <p>For charging at home, Type 1 and Type 2 are the most used connections between the charger and the vehicle. The connector type you will need will be determined by your EV. Type 1 connectors are currently favored by the Asian car manufacturers such as Nissan and Mitsubishi, while most American and European manufacturers such as Audi, BMW, Tesla, Renault, Mercedes, VW, and Volvo, use Type 2 connectors. Type 2 is rapidly becoming the most popular charging connection, though.</p>
+      <p>Type 1 connectors have a five-pin plug and Type 2 connectors have a seven-pin plug.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
