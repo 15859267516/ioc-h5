19ab15a1d63568b219991994fb43f384259c1556
15859267516 <271743196@qq.com>
2021-10-09 14:39:11 +0800

    Add files via upload

---
+++ ioc-agreements.vue
@@ -0,0 +1,95 @@
+<!--
+ * @Atuhor: Lee
+ * @Date: 2021-10-09 14:05:56
+ * @LastEditTime: 2021-10-09 14:39:02
+ * @FilePath: \vite\src\components\ioc-agreements.vue
+-->
+<template>
+  <div class="page">
+      <h2>General Terms and Conditions of iocharger application</h2>
+      <p>Version dated 25 Jan 2021</p>
+      <p>These General Terms and Conditions can be accessed at www.iocharger.com. If the End-user wishes to install the iocharger application, the End-user must affirm his/her agreement to these General Terms and Conditions.</p>
+      <h3>1. Definitions Relating to the iocharger APPLICATION</h3>
+      <p>The following definitions are used in this chapter. These definitions apply to the entire Contract.</p>
+      <p>1.1 Conditions: these general terms and conditions, including any appendices, relating to the purchase of the iocharger App and access to Charging Infrastructure.</p>
+      <p>1.2 Charge Point Operator: an operator of Charging Infrastructure.</p>
+      <p>1.3 Charging Infrastructure: Compatible Charging Sockets that are connected to the iocharger App.</p>
+      <p>1.4 Compatible Charging Socket: a Charging Socket that is included in the database displayed in the iocharger App and which can be operated via the iocharger App.</p>
+      <p>1.5 End-user: the natural person or legal entity that has installed the iocharger App and that may wish to have access to the Charging Infrastructure</p>
+      <p>1.6 Electric Vehicle: A road vehicle powered entirely by an electric motor and/or a hybrid vehicle partly powered by an electric motor, which vehicle may or may not make use of electricity that is stored in a battery rechargeable by use of a Charging Socket.</p>
+      <p>1.7 Charging Socket: a facility at a semi-public, public, or private location that can be used to charge the battery of an Electric Vehicle.</p>
+      <p>1.8 Charging Session: a session during which an Electric Vehicle is charged at one of the Compatible Charging Sockets using the iocharger App.</p>
+      <p>1.9 Non-compatible Charging Socket: a Charging Socket that is included in the database displayed in the iocharger App but which cannot be operated via the iocharger App.</p>
+      <p>1.10 Contract: the agreement pursuant to which the Charging Infrastructure is made available to the End-user via the iocharger App, of which these general terms and conditions constitute an integral part.</p>
+      <p>1.11 iocharger: XIAMEN GALAXY CAMPHOL TECHNOLOGY CO., LTD, registered at Unit 1404, Building A01, Software Park 3, Jimei, Xiamen, Fujian, China.</p>
+      <p>1.12 Platform: the website from which the iocharger App can be downloaded.</p>
+      <p>1.13 iocharger App: the iOS and Android application offered by iocharger, based on which the End-user can access the Charging Infrastructure in order to charge his/her Electric Vehicle.</p>
+      <p>1.14 Service Provider: a party with which a contract for the supply of charging services is concluded.</p>
+      <h3>2. Installation, Operation, and Use of the iocharger App</h3>
+      <p>2.1 iocharger distributes the iocharger App via the Platform, from which the digital content can be downloaded. To be able to use the iocharger App, visit the Platform, where you will register with the Platform operator. That will bind you to the Platform’s relevant terms and conditions of use and general terms and conditions. The contractual relationship between you and the operator regarding your use of the Platform is separate from the contractual relationship that you enter into directly with iocharger for the use of the iocharger App pursuant to the Contract. If there are problems with using the Platform, you must contact the Platform operator directly in its capacity as your counterparty. Clause 11 (exclusion of liability) applies mutatis mutandis to that contract.</p>
+      <p>2.2 The iocharger App can be downloaded onto a compatible mobile terminal that uses iOS or Android.</p>
+      <p>2.3 Before the iocharger App can be used, the End-user may open an account or log in as a guest. If a user account is opened, the End-user will, independently and at any time, be able to change his/her personal account settings, such as the data regarding the Electric Vehicle, charging card data and/or payment details. In order to open an account, the End-user must have a valid e-mail address.</p>
+      <h3>3. Right of Use</h3>
+      <p>3.1 iocharger grants the End-user permission to use the iocharger App indefinitely to: </p>
+      <ul>
+          <li>•	find and review Compatible and Non-compatible Charging Sockets;</li>
+          <li>•	obtain access to Compatible Charging Sockets, such as by starting, stopping, and paying for the use of the Charging Sockets.</li>
+      </ul>
+      <p>3.2 The licence to use the iocharger App is non-transferable. The following actions are prohibited while using the application:</p>
+      <ul>
+          <li>•	making the application available to third parties via a peer-to-peer or other network or in any other manner;</li>
+          <li>•	leasing out or selling the application;</li>
+          <li>•	granting a sub-licence to use the application; and</li>
+          <li>•	modifying the application, reverse engineering the source code, and/or using the application to develop derivative works.</li>
+      </ul>
+      <p>3.3 iocharger shall be entitled to terminate the licence immediately and at any time if the End-user violates these Conditions or misuses the application, or if the decision is taken to decommission the iocharger App.</p>
+      <p>3.4 The End-user shall be free to terminate the licence with immediate effect and at any time by sending an e-mail support@intelligentchargingsystems.co.uk, requesting the deletion of the relevant account, upon which the right of use shall terminate with immediate effect.</p>
+      <h3>4. Misuse of the Application</h3>
+      <p>4.1 Misuse is defined as: using the iocharger App in any manner that is not described in these Conditions, as well as using the iocharger App in such a way that it infringes upon the rights of one or more third parties. Misuse of the application is a criminal offence. We may report misuse at any time to the police and/or public prosecutor’s office.</p>
+      <h3>5. Use of Information / Privacy</h3>
+      <p>5.1 By installing the iocharger App, the End-user agrees to having the information he/she enters processed, stored, and used to the extent reasonably necessary for the proper operation of the iocharger App, the further development of the iocharger App, and the offering of access to the Charging Infrastructure via the iocharger App. iocharger shall process, store, and use the information provided by the End-user in accordance with all applicable laws.</p>
+      <h3>6. Location Data</h3>
+      <p>6.1 For the iocharger App to operate, iocharger must be kept apprised of the End-user’s location. iocharger can use this data to direct the End-user to a nearby Compatible Charging Socket. To do so, iocharger must be able to ascertain the End-user’s location in real time. This data is tracked in anonymised form and is never stored.</p>
+      <h3>7. Indemnification</h3>
+      <p>7.1 By installing the iocharger App, the End-user agrees to indemnify iocharger, its directors, supervisory directors, employees, investors, contractors and contractual counterparties against any liability to, or claims instituted by, third parties that arise as the direct or indirect consequence of the End-user’s culpable failure to satisfy these Conditions. The End-user hereby waives any right of recovery he/she has in respect the aforementioned parties.</p>
+      <p>7.2 The End-user is aware of, and agrees that, he/she bears the risk of using the iocharger App (iocharger accepts no liability in respect of said use). The usefulness and quality of the iocharger App depends on its being used properly by the End-user. iocharger does not guarantee the operation or quality of the iocharger App, the correctness or accuracy of the information provided by the iocharger App, or the speed or quality of any information provided in connection with the iocharger App. iocharger is not obliged to update or maintain the iocharger App. The foregoing is related to, inter alia, the fact that the iocharger App is dependent on the use of the Internet, as well as the possibility of receiving text and other messages and obtaining a GPS location, as well as the mobile telephone, mobile and other networks, and third-party services being in good working order. To facilitate the operation of the iocharger App, the End-user will be responsible for, inter alia, the following:</p>
+      <ul>
+          <li>•	the correct provision of information, including (but not limited to) his/her e-mail address, information about his/her Electric Vehicle, and his/her charging card and payment details. The End-user must enter changes in this information himself/herself via his/her personal iocharger App account;</li>
+          <li>•	exercising due care in respect of his/her password for access to the iocharger App and any access codes for using the compatible mobile telephone;</li>
+          <li>•	misuse of the iocharger App upon loss or theft of the compatible mobile telephone;</li>
+          <li>•	the use of the GPS functionality in the iocharger App.</li>
+      </ul>
+      <h3>8. Changes to the Application</h3>
+      <p>8.1 iocharger is entitled to change the iocharger App (for example, in the case of an update or when so prompted by changes in the application or to the applicable laws (or their interpretation), as well as to discontinue or terminate the app without any prior notice being required. iocharger shall not be liable for any harm or loss that ensues, or is alleged to have ensued, from such change, discontinuation, or termination.</p>
+      <h3>9. Intellectual Property</h3>
+      <p>9.1 The End-user is aware that the iocharger App contains information of which iocharger is the owner, including processes, work methods, software, drawings, texts, designs, and know- how, including but not limited to the rights referred to in the UK Intellectual Property ACT 2014, the Benelux Convention on Intellectual Property, the European Union Trademark Regulation, the European Patent Convention, the Uniform Benelux Act on Designs and Models, and the regulations relating to said acts (if not yet mentioned).</p>
+      <p>9.2 The End-user shall respect these rights and use the aforementioned information exclusively for the ordinary use of the application as described in these Conditions.</p>
+      <h3>10. Charging</h3>
+      <p>10.1 The iocharger App will enable the End-user to start, stop, and schedule charging at the Charging Sockets.</p>
+      <p>10.2 When using the Charging Sockets, the End-user shall comply with all requirements applicable to charging; specifically, the requirements imposed by iocharger, as well as any applicable statutory safety and other requirements.</p>
+      <p>10.3 iocharger does not guarantee Charging Infrastructure density and/or the adequate availability of Charging Sockets. Neither does iocharger guarantee that the Charging Sockets will be operational at all times without any disruption and/or failures or that these will be available.</p>
+      <h3>11. Liability</h3>
+      <p>11.1 The End-user shall be liable for any harm or loss resulting from the inexpert or careless use of a Charging Socket and shall indemnify iocharger against any third-party claims relating thereto. The End-user shall also indemnify iocharger against any third-party claims relating to conduct or circumstances for which the End-user bears the risk and/or expense.</p>
+      <p>11.2 iocharger shall be liable for any harm or loss the End-user incurs as a consequence of iocharger’s culpable failure to perform its contractual obligations to the End-user, subject to the condition that, within 10 business days after the date on which the End-user discovers, or could reasonably be expected to have discovered, the failure to perform or wrongful act, the End-user notifies iocharger of same in writing, thereby, to the extent legally required, serving iocharger with notice of default and affording iocharger a reasonable time to perform its obligation or rectify its wrongful act.</p>
+      <p>11.3 The liability referred to in the previous paragraph of this clause shall – if and to the extent possible and with due observance of mandatory liability law – be limited to damages for direct harm or loss. iocharger will not be liable for any loss of business, use, profit, anticipated profit, contracts, revenues, goodwill or anticipated savings; loss of data or use of data; or damage to the End User´s reputation or any costs relating thereto.</p>
+      <p>11.4 iocharger shall not be liable for any harm or loss the End-user may incur as a result of being unable to charge (or fully charge) the Electric Vehicle or as a result of the use or operation of a Charging Socket. iocharger shall not be liable if an Electric Vehicle cannot be charged (or safely charged) due to a defect in the Electric Vehicle and/or any of the auxiliary equipment used, such as charging cables.</p>
+      <p>11.5 The restrictions listed in the previous paragraphs of this clause shall lapse if and to the extent that the harm or loss is the consequence of an intentional act or omission or gross negligence on the part of iocharger.</p>
+      <p>11.6 iocharger shall not be held liable to perform any obligation if it is prevented from doing so as a result of force majeure.</p>
+      <p>11.7 iocharger’s terms and conditions apply to payments made via their payment service.</p>
+      <h3>12. Other Provisions and Applicable Law</h3>
+      <p>12.1 iocharger shall at all times be authorised to amend these Conditions on a unilateral basis.</p>
+      <p>12.2 In performing this Agreement, iocharger shall be entitled to engage third parties or to assign all or part of the rights and obligations under this Agreement to a third party. The End-user hereby consents in advance to the assignment of its legal relationship with iocharger to a third party.</p>
+      <p>12.3 If, for whatever reason, one or more provisions of these Conditions is invalid or null and void or is nullified, the remaining provisions shall remain in full force, and the Parties shall consult in order to put in place a replacement provision such that the intention and purport of the provision that is to be replaced is preserved as far as possible.</p>
+      <p>12.4 This Contract and these Conditions governed by and construed in accordance with the laws of China. Any dispute arising out or in connection with this Contract and these Conditions shall be subject to the exclusive jurisdiction of the competent court in Xiamen, China.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
