d756bcb95d3d27c9cf4e42e230cc306acc284a90
15859267516 <271743196@qq.com>
2021-10-09 15:32:10 +0800

    Add files via upload

---
+++ ioc-privacy.vue
@@ -0,0 +1,132 @@
+<!--
+ * @Atuhor: Lee
+ * @Date: 2021-10-09 14:05:32
+ * @LastEditTime: 2021-10-09 15:25:17
+ * @FilePath: \vite\src\components\ioc-privacy.vue
+-->
+<template>
+  <div class="page">
+    <h2>iocharger Privacy Statement</h2>
+    <h3>1. INTRODUCTION</h3>
+    <p>This Privacy Statement provides information on processing of personal data by XIAMEN GALAXY CAMPHOL TECHNOLOGY CO., LTD., hereafter iocharger, we or us.</p>
+    <p>In this Privacy Statement we describe who we are, how, when and for which purposes we process your personal data, how you can exercise your privacy rights and all other information that may be relevant to you.</p>
+    <p>This Privacy Statement may be changed over time. The most up-to-date Privacy Statement is published on www.iocharger.com. This Privacy Statement applies since Jan 25th, 2021.</p>
+    <h3>2. WHEN DOES THIS PRIVACY STATEMENT APPLY?</h3>
+    <p>iocharger processes personal data for the purposes as indicated in this Privacy Statement. Those personal data depend on the service provided by iocharger, for more information on these services and to understand which service(s) iocharger delivers to you, please see www.iocharger.com.</p>
+    <p>This is the Privacy Statement for customer and/or client (also referred to as; user) data, in so far as this data is personal data processed within iocharger2.0Lite software environment. This Privacy Statement also applies to the processing of personal data within that environment via our iocharger2.0Lite app. Please note that this Privacy Statement does not address the processing of personal data of individuals by iocharger outside of those environments.</p>
+    <h3>3. WHO IS RESPONSIBLE FOR YOUR PERSONAL DATA?</h3>
+    <p>INTELLIGENT CHARGING SYSTEMS is the controller of the processing of all personal data that fall within the scope of this Privacy Statement. This Privacy Statement indicates what personal data are processed by iocharger and for what purposes, and to which persons or entities, that data may be shared. iocharger may share your personal data with external parties insofar as this is indicated in this Privacy Statement.</p>
+    <h3>4. WHICH CATEGORIES OF PERSONAL DATA DO WE PROCESS ABOUT YOU?</h3>
+    <p>In order to offer you our services as Charge Point Operator (CPO) and Electro Mobility Service Provider (EMSP), including through the use of our iocharger2.0Lite app, the following categories of personal data are processed:</p>
+    <ul>
+        <li>• Contact details (such as name, address, telephone number, email address)</li>
+        <li>• Vehicle details (such as type, colour, license plate)</li>
+        <li>• User location details (such as real time user location information)</li>
+        <li>• Transaction details (such as charging card ID number, technical charging information, charging station location information)’</li>
+    </ul>
+    <h3>5. HOW DO WE COLLECT YOUR PERSONAL DATA?</h3>
+    <p>iocharger collects your personal data:</p>
+    <ul>
+        <li>• Directly from you through the use of our products and/or services, including via the use of our iocharger2.0Lite app; or</li>
+        <li>• When you purchase a EV Charger through one of our partners we may receive your personal data from those partners after they have received your consent for doing so; </li>
+    </ul>
+    <h3>6. FOR WHICH PURPOSES DO WE PROCESS YOUR PERSONAL DATA</h3>
+    <p>The following scenarios set out when and for which purposes iocharger processes your personal data.</p>
+    <h4>6.1 When you use our products and/or services</h4>
+    <h5>a) To register you as a user of our products and/or services, administrate your account and to ensure the confidentiality and security of your account</h5>
+    <p>For you to be able to make use of our products and/or services, your personal data is entered into our systems in order for you to make use of them. We will also administrate your account and ensure that the information on your account is confidential and adequately secured.</p>
+    <p>For this purpose, we process the following personal data categories:</p>
+    <ul>
+        <li>• Contact details (such as name, address, telephone number, email address)</li>
+        <li>• Vehicle details (such as type, colour, license plate)</li>
+    </ul>
+    <p>This purpose has a legal basis under Article 6(1)(b) and (f) GDPR, processing is necessary for the performance of contract and for the purposes of a legitimate interest pursued by iocharger.</p>
+    <p>iocharger has a legitimate interest to process your personal data to make sure your account details remain confidential and secure</p>
+    <h5>b) To deliver to you our products and/or services, their functionalities and for their technical, functional and operational management</h5>
+    <p>If you use our product and/or service, we process personal data to offer you our products and/or services, their functionalities and to allow our product administrators to manage our product’s and/or service’s performance. We also process your personal data in order to provide you with necessary service and maintenance of our products and/or service. When you use the iocharger2.0Lite app and when you have given consent, we also use your real time location in order to be to provide you all the services of our app.</p>
+    <p>For this purpose, we process the personal data that is generated by your use of our products and/or services.</p>
+    <p>We process the following personal data categories:</p>
+    <ul>
+        <li>• Contact details (such as name, address, telephone number, email address)</li>
+        <li>• Vehicle details (such as type, colour, license plate)</li>
+        <li>• User location details (such as user location information)</li>
+    </ul>
+    <p>This purpose has a legal basis under Article 6(1)(a) GDPR and Article 6(1)(b) GDPR, processing is based on the user’s consent and is necessary for the performance of contract.</p>
+    <h3>c) For the development and improvement of our products and/or services</h3>
+    <p>We process your personal data in order to assess, analyze and improve our products and (customer) services. We use aggregated personal data to analyze customer accounts and to adjust our products and services accordingly. When you use our products and/or services, we also process your personal data to compile analysis reports for internal development purposes.</p>
+    <p>For this purpose, we process the following personal data categories about you:</p>
+    <ul>
+        <li>• Contact details (such as name, address, telephone number, email address)</li>
+        <li>• Transaction details (such as charging card ID number, technical charging information, charging station location information)</li>
+    </ul>
+    <p>This purpose has a legal basis under Article 6(1)(f) GDPR, processing is necessary for the purposes of a legitimate interest pursued by iocharger.</p>
+    <p>iocharger has a legitimate interest to process aggregated personal data to improve our products and (customer) services.</p>
+    <h4>6.2 When you interact with us</h4>
+    <h5>a) To communicate with you (online and offline)</h5>
+    <p>If you get in touch with us via our contact details or via our website or iocharger2.0Lite app, we will use your personal data in order to reply and answer your question.</p>
+    <p>For this purpose, we process your name, contact details, your correspondence with us, your question and all other personal data which are provided by you or necessary to answer your question.</p>
+    <p>This purpose has a legal basis under Article 6(1)(f) GDPR, processing is necessary for the purposes of a legitimate interest pursued by iocharger.</p>
+    <p>iocharger has a legitimate interest to process your personal data to be able to communicate with you.</p>
+    <h4>6.3 To comply with the law</h4>
+    <p>In some cases, we process your personal data to comply with applicable laws and regulations. Also, in order to comply with relevant laws and regulations, we may need to disclose your personal data as a consequence of a request of a regulatory agency or supervisory authority.</p>
+    <p>For this purpose, we may process the following personal data categories about you:</p>
+    <ul>
+        <li>• Contact details (such as name, address, telephone number, email address)</li>
+        <li>• Vehicle details (such as type, colour, license plate)</li>
+        <li>• Transaction details (such as charging card ID number, technical charging information, charging station location information)</li>
+    </ul>
+    <p>This purpose has a legal basis under Article 6(1)(c) GDPR, processing is necessary for the compliance with a legal obligation.</p>
+    <h3>7. WHO HAS ACCESS TO YOUR PERSONAL DATA?</h3>
+    <h4>7.1 Access to your personal data within iocharger</h4>
+    <p>We ensure that your personal data is shared internally only to the extent necessary to serve the purposes set out in this Privacy Statement. Our employees are aware of the need to respect your privacy and are authorized only to access personal data when it is necessary for them to carry out their respective functions.</p>
+    <h4>7.2 Access to your personal data by external parties</h4>
+    <p>The following categories of external parties may have access to personal data, only if relevant, for the provisioning of their products or services to us:</p>
+    <ul>
+        <li>• Business partners of iocharger (to register you as a user of and to deliver to you our products and/or services)</li>
+        <li>• Charging point manufacturers (to provide for service and maintenance of our products and/or services)</li>
+        <li>• IT partners (to provide technical</li>
+        <li>• Telecom providers (to provide for online telecommunications between charging points and data centers)</li>
+        <li>• Cloud providers (to provide storage for data processed by iocharger)</li>
+    </ul>
+    <p>When external parties are given access to your personal data, we will take the required contractual, technical and organizational measures to ensure that your personal data are only used by those external parties to the extent that such use is necessary for the purposes set out in this Privacy Statement. The external parties may only access your personal data in accordance with contractual obligations with iocharger and applicable law.</p>
+    <p>If your personal data are transferred to an external party in a country outside of the European Economic Area (EEA) that does not provide an adequate level of protection for personal data, we take measures to ensure that your personal data are adequately protected. We do this by entering into EU Standard Contractual Clauses with these external parties or by ensuring that appropriate safeguards are in place such as the EU-US Privacy Shield Framework.</p>
+    <p>In other cases, your personal data will not be supplied to external parties, except where required or permitted by law.</p>
+    <h4>7.3 The use of your personal data by data processors</h4>
+    <p>When an external party processes your personal data solely following iocharger’ instructions, it acts as a data processor. Prior to interacting with such data processors, we enter into a data processing agreement with them for the processing of personal data. In this agreement we include obligations to ensure that your personal data are processed by the data processor solely to provide services to us and that this is done subject to the same level of protection as awarded to you by iocharger.</p>
+    <h3>8. HOW ARE YOUR PERSONAL DATA SECURED?</h3>
+    <p>iocharger has taken adequate safeguards to ensure the confidentiality and security of your personal data. We have implemented appropriate technical, physical and organizational measures to protect personal data against accidental or unlawful destruction or accidental loss, damage, alteration, unauthorized disclosure or access as well as all other forms of unlawful processing (including, but not limited to, unnecessary collection) or further processing.</p>
+    <h3>9. HOW LONG ARE YOUR PERSONAL DATA RETAINED?</h3>
+    <p>iocharger ensures that it retains personal data only if it is necessary for specific purposes. Your personal data will be deleted or made anonymous when your personal data is no longer necessary for the purposes for which these personal data were collected.</p>
+    <h3>10. HOW CAN YOU EXERCISE YOUR PRIVACY RIGHTS?</h3>
+    <p>You have the right to request access of an overview or copy of your personal data, and under certain conditions, rectification and/or erasure of personal data. In addition, you may also have the right of restriction of processing concerning your personal data, the right to object to processing as well as the right to data portability.</p>
+    <p>To invoke your right of access, rectification, and/or erasure of personal data, your right of restriction of processing, and/or your right to object to processing as well as to invoke your right to data portability, please contact us by using the contact details at the bottom of this Privacy Statement. Please keep in mind that we may ask for additional information to verify your identity.</p>
+    <p>If you have given your consent to a certain purpose, you can withdraw your consent at any time. Please keep in mind that withdrawal does not have retroactive effect. You can contact us by using the contact details at the bottom of this Privacy Statement.</p>
+    <h3>11. CAN YOU LODGE A COMPLAINT?</h3>
+    <p>You can lodge a complaint with your local data protection supervisory authority when you have a complaint about the use of your personal data by iocharger. For example, if you believe that we do not process your personal data carefully, or because you have sent us a request to access or rectification of your personal data and you are not satisfied with our reply or we did not reply in a timely manner.</p>
+    <p>The contact details of the Dutch data protection supervisory authority are:</p>
+    <ul>
+        <li>Autoriteit Persoonsgegevens</li>
+        <li>Bezuidenhoutseweg 30</li>
+        <li>2594 AV The Hague</li>
+        <li>Netherlands</li>
+    </ul>
+    <h3>12. HOW CAN YOU CONTACT US?</h3>
+    <p>If you have any questions about the way we process your personal data, please read this Privacy Statement first. For additional questions or complaints, please contact:</p>
+    <ul>
+        <li>XIAMEN GALAXY CAMPHOL TECHNOLOGY CO., LTD.</li>
+        <li>Unit 04,14th floor,Building A1,Software Park 3, </li>
+        <li>Jimei, XiaMen, Fujian, China</li>
+        <li>support@iocharger.com</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
